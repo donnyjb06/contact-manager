cb7a9a0b16ae353e1c4795a710d92cf659182379
Donovan Jabbar <djabbar51@gmail.com>
2025-07-04 20:06:27 -0600

    refactor: make controllers async/await functions

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -1,22 +1,22 @@
 import { RequestHandler } from "express";
 
-const getAllContacts: RequestHandler = (req, res) => {
+const getAllContacts: RequestHandler = async (req, res) => {
   res.status(200).json({message: "Get all contacts"})
 }
 
-const getContact: RequestHandler = (req, res) => {
+const getContact: RequestHandler = async (req, res) => {
   res.status(200).json({message: `Get individual contact ${req.params.id}`})
 }
 
-const postNewContact: RequestHandler = (req, res) => {
+const postNewContact: RequestHandler = async (req, res) => {
   res.status(200).json({message: "Post new contact"})
 }
 
-const deleteContact: RequestHandler = (req, res) => {
+const deleteContact: RequestHandler = async (req, res) => {
   res.status(200).json({message: `Delete contact ${req.params.id}`})
 } 
 
-const updateContact: RequestHandler = (req, res) => {
+const updateContact: RequestHandler = async (req, res) => {
   res.status(200).json({message: `Update contact ${req.params.id}`})
 } 
 
