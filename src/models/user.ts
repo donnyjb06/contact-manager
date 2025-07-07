f4a9ca08e95284d92c7549fa9e909741186800f2
Donovan Jabbar <djabbar51@gmail.com>
2025-07-06 22:26:22 -0600

    feat: implement user schema and model

---
+++ src/models/user.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const userSchema: Schema = new Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email address"],
+    unique: [true, "Email address already exists"],
+    validate: {
+      validator: (v: string) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: (props: any) => `${props.value} is not a valid email address`,
+    },
+  },
+  password: {
+    type: String,
+    minlength: [4, "Password must be at least 4 characters"],
+    maxlength: [20, "Password must be less than 20 characters"],
+    required: [true, "Please provide the user's password"]
+  }
+}, {timestamps: true})
+
+export const User = model("user", userSchema)
